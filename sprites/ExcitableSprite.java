db88069b00ed5d26653ab80ada6118eb3570b455
gjwehnes <gjwehnes@shaw.ca>
2021-05-06 14:52:55 -0600

    Initial Commit

---
+++ sprites/ExcitableSprite.java
@@ -0,0 +1,12 @@
+
+public interface ExcitableSprite {
+
+	public boolean getIsHappy();
+	public void setIsHappy(boolean isHappy);
+		
+	public boolean getIsEnergetic();
+	public void setIsEnergetic(boolean isEnergetic);
+
+	public void dance(double seconds);
+	
+}
