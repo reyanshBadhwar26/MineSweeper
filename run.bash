c1ffc6cb23af02f51f948520cf69e04aef1851b1
Gustaaf Wehnes <>
2021-05-06 21:40:10 +0000

    simplified classpath

--- run.bash
+++ run.bash
@@ -2,4 +2,4 @@
 echo "compile"
 javac -classpath . utilities/*.java backgrounds/*.java sprites/*.java universes/*.java  gui/*.java main/*.java -d ./bin
 echo "run Main.java"
-java -classpath .:/run_dir/junit-4.12.jar:/run_dir/hamcrest-core-1.3.jar:/run_dir/json-simple-1.1.1.jar:./bin Main
+java -classpath ./bin Main
